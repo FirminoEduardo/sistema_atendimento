ac9e5822488a37566d0672b8d5d81bcc9d06fae3
FirminoEduardo <dudu.lf@outlook.com>
2025-04-24 17:36:12 -0300

    Alteração no core/senha.py - agora com tipo de senha

--- core/senha.py
+++ core/senha.py
@@ -1,6 +1,12 @@
 class Senha:
-    def __init__(self, codigo: str):
+    def __init__(self, codigo: str, tipo: str = "comum"):
         self.codigo = codigo
+        self.tipo = tipo # "comum" ou "prioritaria"
 
     def __str__(self):
         return self.codigo
+    
+    def __str__(self):
+        prefixo = "P" if self.tipo == "prioritaria" else "N"
+        return f"{prefixo}{self.codigo}" 
+    
